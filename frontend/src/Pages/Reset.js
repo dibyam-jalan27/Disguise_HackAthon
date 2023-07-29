c0a1038baf24af19e108d0cbb51543b2320a998d
dibyam-jalan27 <dibyam1jalan@gmail.com>
2023-07-29 16:46:32 +0530

    nav

--- frontend/src/Pages/Reset.js
+++ frontend/src/Pages/Reset.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Reset = () => {
+  const navigate = useNavigate();
   const { token } = useParams();
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
