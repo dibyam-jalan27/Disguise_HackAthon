2cf01f2350c92e8b3cc03373c0d1a1a025c6e573
Pratham Sahu <prathamsahu72@gmail.com>
2023-07-30 22:12:31 +0530

    it. page

--- frontend/src/Pages/ItineraryPage.js
+++ frontend/src/Pages/ItineraryPage.js
@@ -1,62 +1,129 @@
-import React, { useEffect, useState } from 'react';
-import Card4 from '../components/Card4';
 import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
 
-const ItineraryPage = ({}) => {
+const ItineraryPage = () => {
 
-  const [ keyword, setKeyword ] = useState("");
-  const [data,setData] = useState([]);
-  const [arr , setArr] = useState([]);
-  // console.log(data);
-  useEffect(()=>{
-    axios.get(`/api/v1/city?keyword=${keyword}`).then((res)=>{
-      setData(res.data.cities);
-      console.log(res.data.cities);
+  const [items, setItems] = useState([]);
+  const [isCityModalOpen, setIsCityModalOpen] = useState(false);
+  const [selectedCity, setSelectedCity] = useState('');
+  const [date, setDate] = useState('');
 
-      
-      
-    }).catch((err)=>{
-      console.log(err);
-    })
-  },[]);
-
-  
-  useEffect(() => {
-    for (let index = 0 ; index < data.length ; index++) {
-
-      const element = data[index];
-      const obj = { name : element.name , isSelected : false};
-      setArr(arr => [...arr , obj]);
-      console.log("heo");
+  const handleAddItem = () => {
+    if (selectedCity && date) {
+      const newItem = {
+        id: new Date().getTime(),
+        name: selectedCity,
+        date: date,
+      };
+      setItems((prevItems) => [...prevItems, newItem]);
+      setSelectedCity('');
+      setDate('');
+      setIsCityModalOpen(false);
     }
-    // console.log("arr " , arr);
-  } , [data])
-  
-  const handleSelect = async () => {
+  };
 
-  }
+  const openCityModal = () => {
+    setIsCityModalOpen(true);
+  };
+
+  const closeCityModal = () => {
+    setIsCityModalOpen(false);
+  };
+
+  const handleCitySelect = (city) => {
+    setSelectedCity(city);
+    closeCityModal();
+  };
+
+  const [ data , setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/api/v1/city`)
+      .then((res) => {
+        setData(res.data.cities)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      console.log(data);
+  }, []);
+
   return (
-    <div className='bg-slate-400 '>
-      <div className='p-10'>
-      <input
-              className=" rounded-full mt-10 p-4 text-black w-[300px] md:w-[400px] font-serif focus:outline-none my-1 "
-              type="text"
-              placeholder="Search Destination..."
-              onChange={(e) => setKeyword(e.target.value)}
-            />
-      </div>
-      <div className='grid p-4 text-sm mt-5 grid-cols-3 mx-auto md:grid-cols-4'>
-          {data.map((city , index) => {
-      
-            return(
-               <Card4 id={city._id} img={city?.images} title={city.name} description={city.description} rating={city.rating} arr={arr} setArr={setArr} index={index} />
-               )
-              }
-            
+    <div className="container mx-auto py-28 ">
+      <h1 className="text-6xl font-bold mb-4">Itinerary Planner</h1>
+      <div className="grid grid-cols-2 gap-4">
+        {/* Left Column - Items */}
+        <div className='bg-gray-200 rounded-full px-10'>
+          <h2 className="text-xl font-bold mb-2">Itinerary Items</h2>
+          {items.length === 0 ? (
+            <p>No items added to the itinerary yet.</p>
+          ) : (
+            <ul>
+              {items.map((item) => (
+                <li key={item.id} className="mb-2">
+                  <span className="font-semibold">{item.name}</span> - {item.date}
+                </li>
+              ))}
+            </ul>
           )}
+        </div>
+
+        {/* Right Column - Form */}
+        <div>
+          <h2 className="text-xl font-bold mb-2">Add New Item</h2>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={openCityModal}
+          >
+            Select City
+          </button>
+          <Modal
+            isOpen={isCityModalOpen}
+            onRequestClose={closeCityModal}
+            contentLabel="City Selector"
+            className="modal"
+            overlayClassName="overlay"
+          >
+            <h2 className="text-xl font-bold mb-4">Select City</h2>
+            <div className="flex flex-wrap">
+                {
+                  data.map((city) => {
+                <button
+                className="bg-gray-200 px-3 py-2 rounded m-2"
+                onClick={() => handleCitySelect('New York')}
+                  >
+                {city.name}
+              </button>
+                  })
+                }
+              
+              
+              {/* Add more city buttons as needed */}
+            </div>
+          </Modal>
+          {selectedCity && (
+            <div className="mt-4">
+              <p className="font-semibold">Selected City: {selectedCity}</p>
+              <input
+                type="text"
+                placeholder="Date"
+                className="border border-gray-300 rounded px-3 py-2 w-full mt-2 focus:outline-none focus:border-blue-500"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
+                onClick={handleAddItem}
+              >
+                Add Itinerary
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-
   );
 };
 
