911c357cb9663a6b3a3e794c265935d31f610674
dibyam-jalan27 <dibyam1jalan@gmail.com>
2023-07-30 15:05:00 +0530

    fixed navbar

--- frontend/src/Pages/Location.js
+++ frontend/src/Pages/Location.js
@@ -3,104 +3,118 @@ import React from "react";
 import Rating from "../components/rating";
 import axios from "axios";
 import Card2 from "../components/Card2";
-import img1 from "../asset/1.jpg"
-import img2 from "../asset/2.jpg"
-import img3 from "../asset/3.jpg"
-import {useEffect, useState} from "react"
-import {useParams} from "react-router-dom"
-import img4 from "../asset/4.jpg"
+import img1 from "../asset/1.jpg";
+import img2 from "../asset/2.jpg";
+import img3 from "../asset/3.jpg";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import img4 from "../asset/4.jpg";
 
 const Location = () => {
-  //const {id} = useParams();
-  const id = "64c51b553936d5188510f8e0";
+  const {id} = useParams();
+  //const id = "64c51b553936d5188510f8e0";
   const [city, setCity] = useState();
 
+  const data = [
+    {
+      id: 1,
+      img: img1,
+      title: "Long sleeve T-Shirt",
+      isNew: true,
+      oldPrice: 200,
+      price: 170,
+    },
+    {
+      id: 2,
+      img: img2,
+      title: "Long sleeve T-Shirt",
+      isNew: true,
+      oldPrice: 250,
+      price: 180,
+    },
+    {
+      id: 3,
+      img: img3,
+      title: "Long sleeve T-Shirt",
+      isNew: false,
+      oldPrice: 300,
+      price: 183,
+    },
+    {
+      id: 4,
+      img: img4,
+      title: "Long sleeve T-Shirt",
+      isNew: false,
+      oldPrice: 410,
+      price: 350,
+    },
+  ];
 
-  const data =[
-        {
-            id : 1 ,
-            img: img1,
-            title : "Long sleeve T-Shirt",
-            isNew : true ,
-            oldPrice : 200 ,
-            price : 170
-        },
-        {
-            id : 2 ,
-            img: img2,
-            title : "Long sleeve T-Shirt",
-            isNew : true ,
-            oldPrice : 250 ,
-            price : 180 
-        },
-        {
-            id : 3 ,
-            img: img3,
-            title : "Long sleeve T-Shirt",
-            isNew : false ,
-            oldPrice : 300 ,
-            price : 183
-        },
-        {
-            id :  4 ,
-            img: img4,
-            title : "Long sleeve T-Shirt",
-            isNew : false ,
-            oldPrice : 410 ,
-            price : 350
-        }
-    ]
+  if(city){
+    axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&exclude={part}&appid=8a5104fd27599f05f19a7151e1b1ace0`).then((res)=>{
+    console.log(res.data);
+  }).catch((err)=>{
+    console.log(err.response.data.message);
+  })
+}
 
-    useEffect(() => {
-        axios.get(`/api/v1/city/${id}`).then((res)=>{
-          setCity(res.data.city)
-        }).catch((err)=>{
-          console.log(err);
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get(`/api/v1/city/${id}`)
+      .then((res) => {
+        setCity(res.data.city);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
-    {city && city.name && (
-    <div >
-      <img src={city.images} alt="first img" className="w-full h-full opacity-80" />
-      <div className="absolute top-16 right-16 w-96 h-96 rounded-[20px] bg-gray-300 opacity-60 ">
-        <p className="flex justify-center items-center p-3 text-black text-[2rem]">
-          Weather
-        </p>
-        <div className="flex justify-between p-[20px] text-[30px]">
-          <div>20°c</div>
-          <div>Cloudy</div>
-        </div>
-        <div className="flex justify-between p-[20px] text-[30px] bottom-5">
-          <div>
-            <div>Humidity</div>
-            <div>icon</div>
-          </div>
-            <div>
+      {city && city.name && (
+        <div>
+          <img
+            src={city.images}
+            alt="first img"
+            className="w-full h-full opacity-80"
+          />
+          <div className="absolute top-16 right-16 w-96 h-96 rounded-[20px] bg-gray-300 opacity-60 ">
+            <p className="flex justify-center items-center p-3 text-black text-[2rem]">
+              Weather
+            </p>
+            <div className="flex justify-between p-[20px] text-[30px]">
+              <div>20°c</div>
+              <div>Cloudy</div>
+            </div>
+            <div className="flex justify-between p-[20px] text-[30px] bottom-5">
+              <div>
+                <div>Humidity</div>
+                <div>icon</div>
+              </div>
+              <div>
                 <div>Pressure</div>
                 <div>icon</div>
+              </div>
             </div>
-      </div>
-    </div>
-      <div>
-      <p className="absolute top-28 left-16 text-[60px] font-medium">{city.name}</p>
+          </div>
+          <div>
+            <p className="absolute top-28 left-16 text-[60px] font-medium">
+              {city.name}
+            </p>
 
-        <Rating/>
-      </div>
-      <div className="absolute top-[500px] left-16 text-[30px] text-4xl text-center text-yellow-800 font-bold text-shadow-lg">
-        {city.description}
-      </div>
-      <div className='flex gap-10 p-5 justify-between'>
-            {data.map((item) => ( 
-                <Card2 item={item} key={item.id}  />
+            <Rating />
+          </div>
+          <div className="absolute top-[500px] left-16 text-[30px] text-4xl text-center text-yellow-800 font-bold text-shadow-lg">
+            {city.description}
+          </div>
+          <div className="flex gap-10 p-5 justify-between">
+            {data.map((item) => (
+              <Card2 item={item} key={item.id} />
             ))}
-            
+          </div>
         </div>
+      )}
     </div>
-      )
-            }
-            </div>
   );
 };
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,38 +1,29 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./NavbarStyle.css";
 import { MenuItems } from "./MenuItems";
-import { IconBase } from "react-icons/lib";
-import { Link} from "react-router-dom";
-import { BiHome } from "react-icons/bi";
-import { FcAbout } from "react-icons/fc";
-import { MdOutlineMedicalServices} from "react-icons/md";
-import { MdPermContactCalendar} from "react-icons/md";
-import {VscAccount} from "react-icons/vsc";
-import {BsSuitHeart} from "react-icons/bs";
-import {AiOutlineLogin} from 'react-icons/ai';
-import {AiOutlineLogout} from 'react-icons/ai';
+import { Link } from "react-router-dom";
+import { VscAccount } from "react-icons/vsc";
+import { BsSuitHeart } from "react-icons/bs";
+import { AiOutlineLogin } from 'react-icons/ai';
+import { AiOutlineLogout } from 'react-icons/ai';
 import axios from "axios";
 
-export default function Navbar() {
-  const [login, setLogin] = React.useState(false);
-  axios.get("/api/v1/me").then((res) => {
-    setLogin(true);
-  }).catch((err) => {
-    setLogin(false);
-  });
+export default function Navbar({ login, setLogin }) {
 
   const logout = () => {
-    axios.get("/api/v1/logout").then((res) => {
-      setLogin(false);
-    }).catch((err) => {
-      console.log(err);
-    });
+    axios.get("/api/v1/logout")
+      .then((res) => {
+        setLogin(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  
+
   return (
     <>
       <nav className="NavbarItems">
-      <h1 className="navbar-logo">Trippy</h1>
+        <h1 className="navbar-logo">Trippy</h1>
         <ul className="nav-menu">
           {MenuItems.map((item, index) => {
             return (
@@ -42,31 +33,27 @@ export default function Navbar() {
             );
           })}
         </ul>
-          
-        <ul className="right">
-          <li>
 
-          </li>
-        {login?(
-          <ul className="singup">
-          <li>
-            <Link to="/me"><VscAccount/></Link>
-          </li>
-          <li>
-            <Link onClick={logout}><AiOutlineLogout/></Link>
-            </li>
+        <ul className="right">
+          {login ? (
+            <ul className="singup">
+              <li>
+                <Link to="/me"><VscAccount /></Link>
+              </li>
+              <li>
+                <Link onClick={logout}><AiOutlineLogout /></Link>
+              </li>
             </ul>
-            ):(
+          ) : (
             <li>
-              <Link to="/login"><AiOutlineLogin/></Link>
+              <Link to="/login"><AiOutlineLogin /></Link>
             </li>
-            )}
-            <li>
-              <BsSuitHeart/>
+          )}
+          <li>
+            <BsSuitHeart />
           </li>
         </ul>
       </nav>
     </>
   );
 }
-
